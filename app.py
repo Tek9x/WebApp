e140844bc7fddadfb26c9e7b9451196f7c565864
Tek9x <none@none.com>
2015-12-12 02:32:28 -0500

    new

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def user_download():
     with app.open_instance_resource('downloaded_file', 'wb') as f:
         f.write(r.content)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
